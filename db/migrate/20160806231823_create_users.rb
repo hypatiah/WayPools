85fd7f96b863327e06c1406bfb4b877493311ecb
Alexander Pellas <email": null>
2016-08-07 18:05:34 -0700

    Fix database and associations.

--- db/migrate/20160806231823_create_users.rb
+++ db/migrate/20160806231823_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-    	t.string :name, presence: true
-    	t.string :email, presence: true, uniqueness: true
-    	t.string :password_digest, presence: true
+      t.string :name, presence: true
+      t.string :email, presence: true, uniqueness: true
+      t.string :password_hash, presence: true
 
     	t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -50,9 +50,9 @@ ActiveRecord::Schema.define(version: 20160806232103) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "password_hash"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
