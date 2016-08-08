c42a0e0b2d5d7beee417b250bf55a06190130abb
raj7desai <raj7desai@gmail.com>
2016-08-07 21:42:42 -0700

    Add JS header in application.erb and add logged_in in user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,13 @@
 module SessionsHelper
   def current_user
-    @current_user ||=User.find(session[:user_id])
+    @current_user ||= User.find(session[:user_id])
   end
 
   def login(user)
     session[:user_id] = user.id
+  end
+
+  def logged_in?
+    return true if session[:user_id]
   end
 end
