4af781d9fbdc073e9d2db8636182ec99c893faa0
Alexander Pellas <email": null>
2016-08-07 17:09:52 -0700

    Fix models and migrations.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# User.create(name: "Bob Saget", email: "bobbysags@gmail.com", password_digest: "123")
+
+# Drive.create(user_id: 1, start_address: "2075 3rd St, San Francisco, CA 94107", end_address: "633 Folsom St, San Francisco, CA 94107", depart_min: '2016-08-07 00:00:00-00:00', depart_max: '2016-08-08 00:00:00-00:00', seats_available: 3, tolerance: 10, description: "I'm super-cool.")
+
+
+
